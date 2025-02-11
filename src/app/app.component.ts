07d7854f11877787c3e8ab0757af3aafa8727be6
MurilloHMS <murillohenriquemelo@outlook.com>
2025-02-11 17:17:22 -0300

    adicionado scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
+import { ScrollToTopComponent } from "./components/scroll-to-top/scroll-to-top.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Portifolio';
+  title = 'MurilloHMS';
 }
