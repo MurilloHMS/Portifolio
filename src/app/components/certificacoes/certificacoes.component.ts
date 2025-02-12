971c2773c01c83a09ea0153ac3c38b4c9253d8de
MurilloHMS <murillohenriquemelo@outlook.com>
2025-02-12 14:34:02 -0300

    criado sessao de certificacoes

--- src/app/components/certificacoes/certificacoes.component.ts
+++ src/app/components/certificacoes/certificacoes.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { certificacoes } from '../../../types/certificacoes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificacoes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './certificacoes.component.html',
   styleUrl: './certificacoes.component.scss'
 })
 export class CertificacoesComponent {
-
+  certificacoes = certificacoes;
 }
---
+++ src/types/certificacoes.ts
@@ -0,0 +1,483 @@
+export const certificacoes = [
+    {
+        nome: "C#: criando e distribuindo bibliotecas em .NET",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "junho de 2024",
+        codigoCredencial: "123456789",
+        urlCredencial: "https://cursos.alura.com.br/certificate/3a5c1259-9be0-437f-96aa-3e5540946315?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Formação C# e Orientação a Objetos: coleções, arquivos e bibliotecas",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "junho de 2024",
+        codigoCredencial: "365eda8d-08e9-419a-944d-bc2da19df85f",
+        urlCredencial: "https://cursos.alura.com.br/degree/certificate/365eda8d-08e9-419a-944d-bc2da19df85f?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Terminal: aprenda comandos para executar tarefas",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "junho de 2024",
+        codigoCredencial: "94dc49ff-bd6b-4a54-b2db-ff0a93b5e59a",
+        urlCredencial: "https://cursos.alura.com.br/certificate/94dc49ff-bd6b-4a54-b2db-ff0a93b5e59a?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: ".NET: criando uma API Web com ASP.NET Core",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "e682428d-cbaf-42a8-8cf3-ee43e5aa2935",
+        urlCredencial: "https://cursos.alura.com.br/certificate/e682428d-cbaf-42a8-8cf3-ee43e5aa2935?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "C#: Trabalhando com Arrays e Coleções",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "11 de junho de 2024",
+        codigoCredencial: "df5684b7-12fb-49b6-99d7-6447ebe88522",
+        urlCredencial: "https://cursos.alura.com.br/certificate/df5684b7-12fb-49b6-99d7-6447ebe88522?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "C#: trabalhando com arquivos",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "3687df79-7522-4a5f-be21-5b66c4a37c76",
+        urlCredencial: "https://cursos.alura.com.br/certificate/3687df79-7522-4a5f-be21-5b66c4a37c76?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Docker: criando e gerenciando containers",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "3ce84a09-7989-49ff-9ab3-301b0eb98b86",
+        urlCredencial: "https://cursos.alura.com.br/certificate/3ce84a09-7989-49ff-9ab3-301b0eb98b86?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Formação Começando com Linux",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "2631ddc7-0b03-4661-88d7-4df889b9297c",
+        urlCredencial: "https://cursos.alura.com.br/degree/certificate/2631ddc7-0b03-4661-88d7-4df889b9297c?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Linux Onboarding: trabalhe com usuários, permissões e dispositivos",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "1b48a898-c229-4aa1-994b-c129800273f2",
+        urlCredencial: "https://cursos.alura.com.br/certificate/1b48a898-c229-4aa1-994b-c129800273f2?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Microsoft SQL Server 2022: conhecendo SQL",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "36f25c52-16c0-44ce-acab-529de90eee10",
+        urlCredencial: "https://cursos.alura.com.br/certificate/36f25c52-16c0-44ce-acab-529de90eee10?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Windows Prompt: utilizando o CMD",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Maio de 2024",
+        codigoCredencial: "d9746baf-72db-4e3d-a133-158ed8c77378",
+        urlCredencial: "https://cursos.alura.com.br/certificate/d9746baf-72db-4e3d-a133-158ed8c77378?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Formação Windows Forms com C#",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Abril de 2024",
+        codigoCredencial: "c5c12a1e-9e2b-440e-a1da-ebeb0bf6c80a",
+        urlCredencial: "https://cursos.alura.com.br/degree/certificate/c5c12a1e-9e2b-440e-a1da-ebeb0bf6c80a?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Linux Onboarding: localizando arquivos e conteúdos",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Abril de 2024",
+        codigoCredencial: "c12092e1-9462-49c5-8877-e9bd226bc3ad",
+        urlCredencial: "https://cursos.alura.com.br/certificate/c12092e1-9462-49c5-8877-e9bd226bc3ad?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Linux Onboarding: obtendo e tratando informações do sistema",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Abril de 2024",
+        codigoCredencial: "dd4311e1-de8a-4e6c-9aa2-dea7206e412d",
+        urlCredencial: "https://cursos.alura.com.br/certificate/dd4311e1-de8a-4e6c-9aa2-dea7206e412d?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "Windows Forms com C#: acesso ao banco de dados SQL Server",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: "Abril de 2024",
+        codigoCredencial: "bd171b20-9110-45dc-846a-1aef7777f06d",
+        urlCredencial: "https://cursos.alura.com.br/certificate/bd171b20-9110-45dc-846a-1aef7777f06d?lang",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    },
+    {
+        nome: "",
+        organizacaoEmissora: "Alura Cursos",
+        dataEmissao: " de 2024",
+        codigoCredencial: "",
+        urlCredencial: "",
+        imagem: "../../../assets/images/alura.png"
+    }
+  ];
+  
