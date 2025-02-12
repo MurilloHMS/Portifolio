7b5094f9f917433369e23b2ce7a72009412c99e9
MurilloHMS <murillohenriquemelo@outlook.com>
2025-02-12 16:49:24 -0300

    incluido componente do whatsapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { ScrollToTopComponent } from "./components/scroll-to-top/scroll-to-top.component";
+import { WhatsappComponent } from "./components/whatsapp/whatsapp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent, ScrollToTopComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, ScrollToTopComponent, WhatsappComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
