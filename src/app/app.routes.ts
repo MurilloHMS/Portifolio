56b002f49d4f3897801492165fcbbce96ce2b06a
MurilloHMS <murillohenriquemelo@outlook.com>
2026-02-04 17:12:25 -0300

    feat: add Kimium Connect project with details and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ControleDeEstoqueProautoComponent } from './pages/controle-de-estoque-proauto/controle-de-estoque-proauto.component';
 import { NgModule } from '@angular/core';
 import { KhoraNoteComponent } from './pages/khora-note/khora-note.component';
+import { KimiumConnectComponent } from './pages/kimium-connect/kimium-connect.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'projetos/controle-estoque', component: ControleDeEstoqueProautoComponent},
-    {path: 'projetos/khora-note', component: KhoraNoteComponent}
+    {path: 'projetos/khora-note', component: KhoraNoteComponent},
+    {path: 'projetos/kimium-connect', component: KimiumConnectComponent},
 ];
 
 @NgModule({
