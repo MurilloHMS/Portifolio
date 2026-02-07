cb90b0301db93fe5092a764d328665b46eda4136
MurilloHMS <murillohenriquemelo@outlook.com>
2026-02-07 09:44:01 -0300

    feat: incluído pagina do site da proauto kimium

--- src/app/pages/site-proauto/site-proauto.component.ts
+++ src/app/pages/site-proauto/site-proauto.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ApresentacaoProjetosComponent } from '../../components/apresentacao-projetos/apresentacao-projetos.component';
 
 @Component({
   selector: 'app-site-proauto',
   standalone: true,
-  imports: [],
+  imports: [ApresentacaoProjetosComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './site-proauto.component.html',
   styleUrl: './site-proauto.component.scss'
 })
